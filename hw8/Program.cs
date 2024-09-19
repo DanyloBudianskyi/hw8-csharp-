db708d4010f8727e7a54ef74a596b181e07629ab
Danil <super.danil501@gmail.com>
2024-09-18 18:15:14 -0600

    Add project files.

---
+++ hw8/Program.cs
@@ -0,0 +1,26 @@
+﻿using Event_practice5;
+
+namespace hw8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo zoo = new Zoo();
+
+            zoo.AnimalAdded += (sender, e) =>
+            {
+                Console.WriteLine($"Animal added. New count: {e.NewCount}");
+            };
+
+            zoo.AnimalRemoved += (sender, e) =>
+            {
+                Console.WriteLine($"Animal removed. New count: {e.NewCount}");
+            };
+
+            zoo.AddAnimal();
+            zoo.AddAnimal();
+            zoo.RemoveAnimal();
+        }
+    }
+}
---
+++ hw8/Zoo.cs
@@ -0,0 +1,47 @@
+﻿namespace Event_practice5
+{
+    public class Zoo
+    {
+        public int AnimalCount { get; private set; }
+        public delegate void ZooEventHandler(object sender, AnimalCountChangedEventArgs e);
+        public event ZooEventHandler? AnimalAdded;
+        public event ZooEventHandler? AnimalRemoved;
+
+        public void AddAnimal()
+        {
+            int oldCount = AnimalCount;
+            AnimalCount++;
+            OnAnimalAdded(new AnimalCountChangedEventArgs(oldCount, AnimalCount));
+
+        }
+        public void RemoveAnimal()
+        {
+            int oldCount = AnimalCount;
+            if (AnimalCount > 0) {
+                AnimalCount--;
+                OnAnimalRemoved(new AnimalCountChangedEventArgs(oldCount, AnimalCount));
+            }
+
+        }
+        public void OnAnimalAdded(AnimalCountChangedEventArgs e)
+        {
+            AnimalAdded?.Invoke(this, e);
+        }
+        protected virtual void OnAnimalRemoved(AnimalCountChangedEventArgs e)
+        {
+            AnimalRemoved?.Invoke(this, e);
+        }
+
+    }
+
+    public class AnimalCountChangedEventArgs : EventArgs
+    {
+        public double OldCount { get; private set; }
+        public double NewCount { get; private set; }
+        public AnimalCountChangedEventArgs(int oldCount, int newCount)
+        {
+            OldCount = oldCount;
+            NewCount = newCount;
+        }
+    }
+}
